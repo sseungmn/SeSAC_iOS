356452218a2d0260cd7d246097b1c6cc5fbd8f40
sseungmn <severck12@naver.com>
2021-10-05 21:13:29 +0900

    Initialize D06_beamin

---
+++ D06_beamin/D06_beamin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  D06_beamin
+//
+//  Created by OHSEUNGMIN on 2021/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
