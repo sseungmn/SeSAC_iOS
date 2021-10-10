e12c5efe97f6a705cda32f7f3820974b20c28a20
sseungmn <severck12@naver.com>
2021-10-11 01:33:50 +0900

    D09_DrinkWater / Implement reset, userDefaults, drinkWater, calcaulateAllowance, updateImageByPercent

--- D09_DrinkWater/D09_DrinkWater/ProfileViewController.swift
+++ D09_DrinkWater/D09_DrinkWater/ProfileViewController.swift
@@ -8,15 +8,18 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     @IBOutlet weak var stageImage: UIImageView!
     @IBOutlet weak var nicknameUserInput: UITextField!
     @IBOutlet weak var heightUserInput: UITextField!
     @IBOutlet weak var weightUserInput: UITextField!
     
-    var nickname: String = ""
-    var height: Int = 0
-    var weight: Int = 0
+    var allowance: Int  { return UserDefaults.standard.integer(forKey: "allowance") }
+    var todayPercent: Int { return  allowance == 0 ? 0 : todayTotal * 100 / allowance }
+    var todayTotal: Int { return UserDefaults.standard.integer(forKey: "total") }
+    var nickname: String  { return UserDefaults.standard.string(forKey: "nickname") ?? "" }
+    var height: Int { return UserDefaults.standard.integer(forKey: "height") }
+    var weight: Int { return UserDefaults.standard.integer(forKey: "weight") }
     
 
     override func viewDidLoad() {
@@ -25,19 +28,66 @@ class ProfileViewController: UIViewController {
         self.navigationController?.navigationBar.tintColor = .white
         self.view.backgroundColor = UIColor(named: "contentDefault")
         
-        getUserData()
-        stageImage.image = UIImage(named: "1-1")
         nicknameUserInput.text = nickname
         heightUserInput.text = String(height)
         weightUserInput.text = String(weight)
     }
     
-    func getUserData() {
-        nickname = UserDefaults.standard.string(forKey: "nickname") ?? "miniOS"
-        height = UserDefaults.standard.integer(forKey: "height")
-        height = height == 0 ? 160 : height
-        weight = UserDefaults.standard.integer(forKey: "weight")
-        weight = weight == 0 ? 50 : weight
+    // view가 보여질 때 마다 바뀌어야 할 것은 따로 정의
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setStageImage()
     }
     
+    @IBAction func saveButtonClicked(_ sender: UIBarButtonItem) {
+        guard let nn = nicknameUserInput.text, let h = Int(heightUserInput.text!), let w = Int(weightUserInput.text!) else {
+            showAlert(of: .UserInputError)
+            return
+        }
+        UserDefaults.standard.set(nn, forKey: "nickname")
+        UserDefaults.standard.set(h, forKey: "height")
+        UserDefaults.standard.set(w, forKey: "weight")
+        showAlert(of: .SaveCompletely)
+    }
+    
+    @IBAction func endEditing(_ sender: Any) {
+        if nicknameUserInput.isEditing || heightUserInput.isEditing || weightUserInput.isEditing {
+            view.endEditing(true)
+        }
+    }
+    
+    func setStageImage() {
+        stageImage.image = getStageImage(todayPercent: todayPercent)
+    }
+    
+    func calculateAllowance() {
+        let newAllowance = (height + weight) * 10 // ml
+        print("new allowance : \(newAllowance)")
+        UserDefaults.standard.set(newAllowance, forKey: "allowance")
+    }
+    
+    func showAlert(of type: AlertType) {
+        let alert: UIAlertController
+        
+        switch type {
+        case .UserInputError:
+            alert = UIAlertController(title: "입력 오류", message: "입력이 올바르지 않습니다.\n다시 한번 확인 해 주세요.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel) { _ in
+                print("Cancel Button Clicked")
+            })
+        case .SaveCompletely:
+            alert = UIAlertController(title: "저장 완료", message: "입력이 올바르게 저장되었습니다.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .default) { _ in
+                print("OK Button Clicked")
+                self.calculateAllowance()
+                self.setStageImage()
+            })
+        }
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    enum AlertType {
+        case UserInputError, SaveCompletely
+    }
 }
--- D09_DrinkWater/D09_DrinkWater/ViewController.swift
+++ D09_DrinkWater/D09_DrinkWater/ViewController.swift
@@ -11,13 +11,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var resetTodayWaterButton: UIBarButtonItem!
     @IBOutlet weak var showProfileViewButton: UIBarButtonItem!
     
-    @IBOutlet weak var todayTotalWaterLabel: UILabel!
-    @IBOutlet weak var todayPercentWaterLabel: UILabel!
+    @IBOutlet weak var todayTotalLabel: UILabel!
+    @IBOutlet weak var todayPercentLabel: UILabel!
     @IBOutlet weak var stageImage: UIImageView!
     
     @IBOutlet weak var userInputWaterLabel: UITextField!
-    @IBOutlet weak var personalWaterDescriptionLabel: UILabel!
-    @IBOutlet weak var drinkWaterButton: UIButton!
+    @IBOutlet weak var allowanceLabel: UILabel!
+    
+    @IBOutlet weak var drinkButton: UIButton!
+    
+    var allowance: Int  { return UserDefaults.standard.integer(forKey: "allowance") }
+    var todayPercent: Int { return  allowance == 0 ? 0 : todayTotal * 100 / allowance }
+    var todayTotal: Int { return UserDefaults.standard.integer(forKey: "total") }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,23 +33,101 @@ class ViewController: UIViewController {
         self.title = "물 마시기"
         
         // font size : 20, 20, 30, 13
-        todayTotalWaterLabel.textColor = .white
-        todayTotalWaterLabel.font = UIFont.systemFont(ofSize: 30, weight: .bold)
-//        UserDefaults.standard.set(0, forKey: "total")
-        todayPercentWaterLabel.textColor = .white
-        todayTotalWaterLabel.text = "0ml"
-        todayPercentWaterLabel.text = "목표의 0%"
+        todayTotalLabel.textColor = .white
+        todayTotalLabel.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        todayPercentLabel.textColor = .white
         
-        stageImage.image = UIImage(named: "1-1")
-        
-        personalWaterDescriptionLabel.text = "안녕하세요님의 하루 물 섭취량은 0L입니다."
+        userInputWaterLabel.keyboardType = .numbersAndPunctuation
     }
     
-    func setTodayWaterLabel() {
-        let allowance = UserDefaults.standard.integer(forKey: "allowance")
-        let total = UserDefaults.standard.integer(forKey: "total")
-        todayTotalWaterLabel.text = String(total)
-        todayPercentWaterLabel.text = String(total / allowance)
+    // View가 보여질 때 마다 해줘야 할 것은 따로 정의
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        update()
+    }
+    
+    // MARK: 물 마시기
+    @IBAction func drinkWaterClicked(_ sender: UIButton) {
+        drinkWater()
+    }
+    // MARK: 키보드 내리기
+    @IBAction func endEditing(_ sender: Any) {
+        if userInputWaterLabel.isEditing { userInputWaterLabel.endEditing(true) }
+    }
+    
+    // MARK: 초기화
+    @IBAction func resetButtonClicked(_ sender: UIBarButtonItem) {
+        UserDefaults.standard.set(0, forKey: "total")
+        UserDefaults.standard.set(0, forKey: "percent")
+        
+        update()
     }
 }
 
+extension ViewController {
+    func update() {
+        setTodayLabel()
+        setAlloanceLabel()
+        setStageImage()
+    }
+    
+    func drinkWater() {
+        // UserInput이 Int형일때만 적용
+        guard let input = Int(userInputWaterLabel.text!) else {
+            // Int형이 아닐 때에는 오류를 표시
+            return;
+        }
+        UserDefaults.standard.set(todayTotal + input, forKey: "total")
+        
+        userInputWaterLabel.text = ""
+        update()
+    }
+    
+    // MARK: 라벨에 적용
+    func setTodayLabel() {
+        todayTotalLabel.text = "\(todayTotal)ml"
+        todayPercentLabel.text = "목표의 \(todayPercent)%"
+    }
+    
+    func setAlloanceLabel() {
+        let Lallowance: Double = Double(allowance) / 1000
+        let nickName = UserDefaults.standard.string(forKey: "nickname") ?? "miniOS"
+        allowanceLabel.text = "\(nickName)님의 하루 물 권장 섭취량은 \(Lallowance)L 입니다."
+    }
+    
+    func setStageImage() {
+        stageImage.image = getStageImage(todayPercent: self.todayPercent)
+    }
+}
+
+func getStageImage(todayPercent: Int) -> UIImage {
+    let UNIT: Double = 100 / 8
+    var stage: Int
+    
+    switch todayPercent {
+    case 0..<Int(1 * UNIT) :
+        stage =  1
+    case Int(1 * UNIT)...Int(2 * UNIT):
+        stage =  2
+    case Int(2 * UNIT)...Int(3 * UNIT):
+        stage =  3
+    case Int(3 * UNIT)...Int(4 * UNIT):
+        stage = 4
+    case Int(4 * UNIT)...Int(5 * UNIT):
+        stage = 5
+    case Int(5 * UNIT)...Int(6 * UNIT):
+        stage = 6
+    case Int(6 * UNIT)...Int(7 * UNIT):
+        stage = 7
+    case Int(7 * UNIT)...Int(8 * UNIT):
+        stage = 8
+    default:
+        stage = 9
+    }
+
+    guard let image = UIImage(named: "1-\(stage)")  else {
+        print("Invalid Image Name")
+        return UIImage()
+    }
+    return image
+}
