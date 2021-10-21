f423e39d34f0abcfd6e51ef347813ad2e806b651
sseungmn <severck12@naver.com>
2021-10-21 20:32:47 +0900

    D13_TrendMedia/ Implement main UI

---
+++ D13_TrendMedia/D13_TrendMedia/MediaPreviewCell.swift
@@ -0,0 +1,39 @@
+//
+//  MediaPreviewCell.swift
+//  D13_TrendMedia
+//
+//  Created by OHSEUNGMIN on 2021/10/21.
+//
+
+import UIKit
+
+class MediaPreviewCell: UITableViewCell {
+    
+    static let identifier = "MediaPreviewCell"
+    @IBOutlet weak var outerPreviewContainer: UIView!
+    @IBOutlet weak var previewContainer: UIView!
+    @IBOutlet weak var previewImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        outerPreviewContainer.layer.masksToBounds = false
+        outerPreviewContainer.backgroundColor = nil
+        outerPreviewContainer.layer.shadowColor = UIColor.systemGray2.cgColor
+        outerPreviewContainer.layer.shadowRadius = 10.0
+        outerPreviewContainer.layer.shadowOpacity = 0.8
+        
+        previewContainer.layer.masksToBounds = true
+        previewContainer.layer.borderWidth = 0
+        previewContainer.layer.cornerRadius = CGFloat(10)
+        
+        previewImage.layer.masksToBounds = false
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- D13_TrendMedia/D13_TrendMedia/ViewController.swift
+++ D13_TrendMedia/D13_TrendMedia/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var mediaTypeContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mediaTypeContainer.layer.cornerRadius = CGFloat(10)
+        mediaTypeContainer.layer.shadowRadius = CGFloat(10)
+        mediaTypeContainer.layer.shadowColor = UIColor.systemGray2.cgColor
+        mediaTypeContainer.layer.shadowOpacity = 0.8
+        
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MediaPreviewCell.identifier) as? MediaPreviewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UIScreen.main.bounds.height / 2
+    }
+    
 
 }
 
