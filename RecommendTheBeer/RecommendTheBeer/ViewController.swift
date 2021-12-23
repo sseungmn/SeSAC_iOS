b5429bc66c133fe54a891e8c1290a00bb43d146a
miniOS <seungmin.oh.dev@gmail.com>
2021-12-23 19:39:25 +0900

    Initialize RecommendTheBeer Project

---
+++ RecommendTheBeer/RecommendTheBeer/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  RecommendTheBeer
+//
+//  Created by SEUNGMIN OH on 2021/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
