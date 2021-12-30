ca2ba76ec2e0a763a19ac2cf38532b4686452aa8
miniOS <seungmin.oh.dev@gmail.com>
2021-12-30 17:30:41 +0900

    chore : restructure directory and make Observable
    Service 디렉토리를 생성해서 API관련 기능을
    옮겨주었고, ViewModel파일을 만들어 두고,
    Observable을 구현했다.

---
+++ RecommendTheBeer/RecommendTheBeer/Observable.swift
@@ -0,0 +1,27 @@
+//
+//  Observable.swift
+//  RecommendTheBeer
+//
+//  Created by SEUNGMIN OH on 2021/12/30.
+//
+
+import Foundation
+
+class Observable<Element> {
+  typealias Listener = ( (Element) -> Void )
+  private var listener: Listener?
+  
+  var value: Element {
+    didSet{
+      listener?(value)
+    }
+  }
+  
+  init(_ value: Element) {
+    self.value = value
+  }
+  
+  func bind(_ completion: @escaping Listener) {
+    listener = completion
+  }
+}
---
+++ RecommendTheBeer/RecommendTheBeer/ViewModel/BeerViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  BeerViewModel.swift
+//  RecommendTheBeer
+//
+//  Created by SEUNGMIN OH on 2021/12/30.
+//
+
+import Foundation
+
+class BeerViewModel {
+  
+}
